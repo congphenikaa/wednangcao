a4be26e6e41f59e9b9225852f1f16b179533113f
Thanhngo2309 <kamenriderlc@email.com>
2025-02-24 17:10:03 +0700

    add admin

--- ecommerce_project/routes/web.php
+++ ecommerce_project/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'home']);
+Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -16,8 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
-Route::get('admin/dashboard',[HomeController::class,'index'])->
-    middleware(['auth','admin']);
-
+Route::get('/admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
