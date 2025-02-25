d1d9f875500e46e9c5e99eb71f8eaea82e9098b6
Cong <you@example.com>
2025-02-25 16:32:13 +0700

    Làm xong phần trang chủ của admin

--- ecommerce_project/resources/views/admin/index.blade.php
+++ ecommerce_project/resources/views/admin/index.blade.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Admin</h1>
+<html>
+    <head> 
+        @include('admin.css')
+    </head>
+    <body>
 
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <input type="submit" value="logout">
-    </form>
+    <!-- Header-->
+        @include('admin.header')
+    <!--End Header-->
+        <!--Sidebar-->
+        @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
 
-</body>
+                @include('admin.body')
+            
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('/admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('/admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('/admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('/admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('/admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('/admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('/admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('/admincss/js/front.js')}}"></script>
+  </body>
 </html>
